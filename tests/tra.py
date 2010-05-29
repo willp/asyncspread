ccd49237ab6f0107f386646749d91e47189a474b
jwillp <jwillp@c2be6c02-a078-7bc6-78f5-0017bf28debf>
2010-05-29 21:12:57 +0000

    * removed debug outputs * added __str__() method for AsyncSpread object, to eliminate confusing "inherited" socket class __str__ method. * added 'map' parameter to permit sharing a single asyncore map among multiple AsyncSpread objects (I don't want to repeat the py2.4 asynchat mistake) * remove OpaqueMessage debug output (now that i have an opaque mesg) * removed extraneous _set_grps() methods in various SpreadMessage subclasses, and made it a parent class method that is overridden by the MembershipMessage classes and inherited by its subclasses. Less code! * updated tra.py test code to make TWO spread connections instead of one and prints out the new __str__() name of the connection

--- tests/tra.py
+++ tests/tra.py
@@ -15,31 +15,43 @@ def ping_response(success, elapsed):
     print 'Client PING callback: Success: %s.  Elapsed time: %.8f' % (success, elapsed)
 
 setup_logging(logging.INFO)
-myname = 'tra-%06d' % (int(time.time()*100) % 1000)
-print 'I am', myname
+myname1 = 'tra1-%05d' % (int(time.time()*100) % 1000)
+myname2 = 'tra2-%05d' % (int(time.time()*100) % 1000)
+print 'I am', myname1, 'and:', myname2
 #
 class MyListener(asyncspread.SpreadPingListener):
     def handle_data(self, conn, message):
         print 'Got message:', message
+        print 'From connection:', conn
 
-    def handle_timer(self, conn):
-        pass
+#    def handle_timer(self, conn):
+#        pass
 
 #listener = asyncspread.SpreadPingListener()
+map=dict()
 listener = MyListener()
-sp = asyncspread.AsyncSpread(myname, sys.argv[1], 24999, listener=listener)
-sp.start_connect()
-
+sp1 = asyncspread.AsyncSpread(myname1, sys.argv[1], 24999, listener=listener,map=map)
+print 'SP1 is:', sp1
+sp2 = asyncspread.AsyncSpread(myname2, sys.argv[1], 24999, listener=listener, map=map)
+sp1.start_connect()
+sp2.start_connect()
+print 'My map:', map
 for g in ('gr1', 'gr2', 'abc123', 'def', 'group2', 'AZ', ''):
-    sp.join(g)
+    sp1.join(g)
+    sp2.join(g)
 loop=0
-while sp.connected:
+while sp1.connected or sp2.connected:
     loop += 1
     print 'client top of loop'
-    sp.multicast(['gr2', 'AZ'], 'my multicast num %d' % (loop), 0, self_discard=False)
-    sp.multicast([], 'To No groups: my multicast num %d' % (loop), 0, self_discard=False)
-    listener.ping(sp, ping_response)
-    sp.loop(50)
+    sp1.multicast(['gr2', 'AZ'], 'my multicast num %d' % (loop), 0, self_discard=False)
+    sp1.multicast([], 'To No groups: my multicast num %d' % (loop), 0, self_discard=False)
+    sp2.multicast(['gr2', 'AZ'], 'SECOND connection: multicast num %d' % (loop), 0, self_discard=False)
+    listener.ping(sp1, ping_response)
+    listener.ping(sp2, ping_response)
+    sp1.loop(50)
+    sp2.loop(50)
     if loop > 500:
-        sp.disconnect()
-        sp.loop(100)
+        sp1.disconnect()
+        sp1.loop(100)
+        sp2.disconnect()
+        sp2.loop(100)

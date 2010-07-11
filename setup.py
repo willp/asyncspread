782b20d3e7dfa4a811efb606080957357b293d18
jwillp <jwillp@c2be6c02-a078-7bc6-78f5-0017bf28debf>
2010-07-11 14:56:21 +0000

    re-ordered and download url

--- setup.py
+++ setup.py
@@ -3,15 +3,18 @@ from distutils.core import setup
 from asyncspread import *
 
 setup (name = 'asyncspread',
-     version = '0.1.1',
-     description = 'Asynchronous Spread Client',
-     long_description = asyncspread.AsyncSpread.__doc__,
-     author = 'J. Will Pierce',
-     author_email = 'willp@nuclei.com',
-     license = 'GNU Lesser General Public License 3 or later',
-     url = 'http://http://code.google.com/p/asyncspread/',
-     packages = ['asyncspread'],
-     keywords = 'spread client asynchronous pubsub channel messaging broker multicast ipc',
+    packages = ['asyncspread'],
+    version = '0.1.1',
+    description = 'Asynchronous Spread Client',
+    author = 'J. Will Pierce',
+    author_email = 'willp@nuclei.com',
+    license = 'GNU Lesser General Public License 3 or later',
+
+    long_description = asyncspread.AsyncSpread.__doc__,
+    url = 'http://http://code.google.com/p/asyncspread/',
+    # may change to a wiki page for downloading the latest version:
+    download_url='http://code.google.com/p/asyncspread/downloads/list',
+    keywords = 'spread client asynchronous pubsub channel messaging broker multicast ipc',
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Environment :: No Input/Output (Daemon)",
@@ -26,6 +29,5 @@ setup (name = 'asyncspread',
         #"Programming Language :: Python :: 2.7",
         "Topic :: System :: Distributed Computing",
         "Topic :: Software Development :: Libraries :: Python Modules"
-    ],
-
-     )
+        ]
+    )

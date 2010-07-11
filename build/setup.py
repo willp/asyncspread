f18702c20707cf9c9100b47c750e38d767e8434a
jwillp <jwillp@c2be6c02-a078-7bc6-78f5-0017bf28debf>
2010-07-11 13:30:54 +0000

    moved back to trunk. may not need build/

--- build/setup.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env python
-from distutils.core import setup
---
+++ trunk/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup (name = "asyncspread",
+     version = "0.1",
+     description = "Asynchronous Spread Client",
+     author = "J. Will Pierce",
+     author_email = "willp@nuclei.com",
+     url = "http://http://code.google.com/p/asyncspread/",
+     packages = ['asyncspread'])

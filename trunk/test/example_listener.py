6d1546886feca1459a0f4b8533c92d035bf355b4
jwillp <jwillp@c2be6c02-a078-7bc6-78f5-0017bf28debf>
2010-09-19 13:37:01 +0000

    more removal of test code

--- trunk/test/example_listener.py
+++ trunk/test/example_listener.py
@@ -2,7 +2,7 @@
 import time, sys, logging
 sys.path.append('.')
 from asyncspread.connection import AsyncSpread
-from asyncspread.listeners import SpreadListener, CallbackListener, GroupCallback
+from asyncspread.listeners import CallbackListener, GroupCallback
 
 def setup_logging(level=logging.INFO):
     logger = logging.getLogger()
@@ -10,9 +10,6 @@ def setup_logging(level=logging.INFO):
     ch = logging.StreamHandler()
     ch.setFormatter(logging.Formatter('%(asctime)s ()- %(levelname)s - %(message)s'))
     logger.addHandler(ch)
-
-def ping_response(success, elapsed):
-    print 'Client PING callback: Success: %s.  Elapsed time: %.8f' % (success, elapsed)
 
 setup_logging(logging.INFO)
 myname = 'HBsrv-%03d' % (int(time.time()*100) % 1000)
@@ -22,21 +19,23 @@ def got_conn(listener, conn):
     print 'Got connected:', conn, '\nJoining the :HB channel...'
     conn.join(':HB')
 def got_dropped(listener, conn):
-    print 'Lost connection to spread server on:', conn.name, 'Reconnecting...'
+    print 'Lost connection to spread server on:', conn.name, 'Reconnecting with start_connect()'
     conn.start_connect()
 def got_error(listener, conn, exc):
     print 'Got exception/error:', str(exc)
     if 'Connection refused' in exc:
         print 'Connection refused by server... Sleeping 1 seconds...'
         time.sleep(1)
-
-def hb_join(conn, group, member, cause):
-    print 'HB join: on group (%s) new member: "%s" joined. Reason: %s' % (group, member, cause)
-def hb_leave(conn, group, member, cause):
-    print 'HB LEAVE: on group (%s) member: "%s" left. Reason: %s' % (group, member, cause)
+#def hb_join(conn, group, member, cause):
+#    print 'HB join: on group (%s) new member: "%s" joined. Reason: %s' % (group, member, cause)
+#def hb_leave(conn, group, member, cause):
+#    print 'HB LEAVE: on group (%s) member: "%s" left. Reason: %s' % (group, member, cause)
 def hb_mesg(conn, message):
-    print '%s>> HB message: %s' % (conn.name, message)
-gcb_hb = GroupCallback(cb_join=hb_join, cb_leave=hb_leave, cb_data=hb_mesg)
+    sender = message.sender
+    data = message.data
+    mtype = message.mesg_type
+    print '%s>> HB message, type=%d, sender="%s", message: %s' % (conn.name, mtype, sender, data)
+gcb_hb = GroupCallback(cb_data=hb_mesg)#cb_join=hb_join, cb_leave=hb_leave, )
 
 listener = CallbackListener(cb_conn=got_conn, cb_dropped=got_dropped, cb_error=got_error)
 listener.set_group_cb(':HB', gcb_hb)

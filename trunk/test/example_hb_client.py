fe1b1409edfd5ff8e84728df8c36ba63ab33afee
jwillp <jwillp@c2be6c02-a078-7bc6-78f5-0017bf28debf>
2010-09-19 13:54:59 +0000

    getting code ready for reuse as example code

--- trunk/test/example_hb_client.py
+++ trunk/test/example_hb_client.py
@@ -12,12 +12,12 @@ def setup_logging(level=logging.INFO):
     logger.addHandler(ch)
 
 setup_logging(logging.INFO)
-myname = 'HBsrv-%03d' % (int(time.time()*100) % 1000)
+myname = 'HBcli-%03d' % (int(time.time()*100) % 1000)
 print 'I am', myname
 
 def got_conn(listener, conn):
     print 'Got connected:', conn, '\nJoining the :HB channel...'
-    conn.join(':HB')
+    #conn.join(':HB')
 def got_dropped(listener, conn):
     print 'Lost connection to spread server on:', conn.name, 'Reconnecting with start_connect()'
     conn.start_connect()
@@ -26,32 +26,24 @@ def got_error(listener, conn, exc):
     if 'Connection refused' in exc:
         print 'Connection refused by server... Sleeping 1 seconds...'
         time.sleep(1)
-def hb_join(conn, group, member, cause):
-    print 'Another HB server joined: on group new member: "%s" joined. Reason: %s' % (member, cause)
-def hb_leave(conn, group, member, cause):
-    print 'A HB server LEFT: on group member: "%s" left. Reason: %s' % (member, cause)
-def hb_mesg(conn, message):
-    conn._hb_count += 1
+
+def got_mesg(listener, conn, message):
     sender = message.sender
     data = message.data
     mtype = message.mesg_type
-    print '%s>> HB message, type=%d, sender="%s", message: %s' % (conn.name, mtype, sender, data)
-    # and send a response back
-    conn.unicast(sender, 'HB Reply:: %d received' % (conn._hb_count), mtype)
-gcb_hb = GroupCallback(cb_data=hb_mesg, cb_join=hb_join, cb_leave=hb_leave)
+    print '%s>> Got message, type=%d, sender="%s", message: %s' % (conn.name, mtype, sender, data)
 
-listener = CallbackListener(cb_conn=got_conn, cb_dropped=got_dropped, cb_error=got_error)
-listener.set_group_cb(':HB', gcb_hb)
+listener = CallbackListener(cb_conn=got_conn, cb_dropped=got_dropped, cb_error=got_error, cb_data=got_mesg)
 
 (host, port) = ('localhost', 24999)
 if len(sys.argv) > 1: host = sys.argv[1]
 if len(sys.argv) > 2: port = int(sys.argv[2])
-hb_srv = AsyncSpread(myname, host, port, listener=listener, start_connect=True)
-print 'hb_srv is:', hb_srv
-hb_srv._hb_count = 0
-
+hb_client = AsyncSpread(myname, host, port, listener=listener, start_connect=True)
+print 'hb_client is:', hb_client
 loop=0
 while loop < 10000:
     print '%s: client top of loop %d' % (myname, loop)
     loop += 1
-    hb_srv.run(10)
+    hb_client.run(10)
+    if loop % 5 == 3:
+        hb_client.multicast([':HB'], 'heartbeat ping from client', loop // 10)

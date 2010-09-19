841ce50b63558ae5060c58b9693a646c82a6d636
jwillp <jwillp@c2be6c02-a078-7bc6-78f5-0017bf28debf>
2010-09-19 15:42:13 +0000

    more changes

--- trunk/test/example_hb_client.py
+++ trunk/test/example_hb_client.py
@@ -11,13 +11,16 @@ def setup_logging(level=logging.INFO):
     ch.setFormatter(logging.Formatter('%(asctime)s ()- %(levelname)s - %(message)s'))
     logger.addHandler(ch)
 
-setup_logging(logging.INFO)
+setup_logging(logging.DEBUG)
 myname = 'HBcli-%03d' % (int(time.time()*100) % 1000)
 print 'I am', myname
 
+(host, port) = ('localhost', 24999)
+if len(sys.argv) > 1: host = sys.argv[1]
+if len(sys.argv) > 2: port = int(sys.argv[2])
+
 def got_conn(listener, conn):
-    print 'Got connected:', conn, '\nJoining the :HB channel...'
-    #conn.join(':HB')
+    print 'Got connected:', conn
 def got_dropped(listener, conn):
     print 'Lost connection to spread server on:', conn.name, 'Reconnecting with start_connect()'
     conn.start_connect()
@@ -27,23 +30,19 @@ def got_error(listener, conn, exc):
         print 'Connection refused by server... Sleeping 1 seconds...'
         time.sleep(1)
 
-def got_mesg(listener, conn, message):
-    sender = message.sender
-    data = message.data
-    mtype = message.mesg_type
-    print '%s>> Got message, type=%d, sender="%s", message: %s' % (conn.name, mtype, sender, data)
+def got_mesg(listener, conn, mesg):
+    print '%s>> Got message, type=%4d, sender="%s", message: %s' % (conn.name, mesg.mesg_type, mesg.sender, mesg.data)
 
 listener = CallbackListener(cb_conn=got_conn, cb_dropped=got_dropped, cb_error=got_error, cb_data=got_mesg)
 
-(host, port) = ('localhost', 24999)
-if len(sys.argv) > 1: host = sys.argv[1]
-if len(sys.argv) > 2: port = int(sys.argv[2])
 hb_client = AsyncSpread(myname, host, port, listener=listener, start_connect=True)
-print 'hb_client is:', hb_client
+
 loop=0
 while loop < 10000:
     print '%s: client top of loop %d' % (myname, loop)
     loop += 1
     hb_client.run(10)
     if loop % 5 == 3:
-        hb_client.multicast([':HB'], 'heartbeat ping from client', loop // 10)
+        try:
+            hb_client.multicast([':HB'], 'heartbeat ping from client', loop // 10)
+        except: print 'cannot send, not connected?'
--- trunk/test/example_hb_server.py
+++ trunk/test/example_hb_server.py
@@ -2,7 +2,7 @@
 import time, sys, logging
 sys.path.append('.')
 from asyncspread.connection import AsyncSpread
-from asyncspread.listeners import CallbackListener, GroupCallback
+from asyncspread.listeners import SpreadListener
 
 def setup_logging(level=logging.INFO):
     logger = logging.getLogger()
@@ -12,46 +12,49 @@ def setup_logging(level=logging.INFO):
     logger.addHandler(ch)
 
 setup_logging(logging.INFO)
+
+class HeartbeatServer(SpreadListener):
+    def __init__(self, name):
+        SpreadListener.__init__(self)
+        self.name = name
+        self.hb_count = 0
+        print 'I am', myname
+
+    def handle_connected(self, conn):
+        print 'Got connected:', conn, '\nJoining the :HB channel...'
+        conn.join(':HB')
+    def handle_dropped(self, conn):
+        print 'Lost connection to spread server on:', conn.name, 'Reconnecting with start_connect()'
+        conn.start_connect()
+    def handle_error(self, conn, exc):
+        print 'Got exception/error:', str(exc)
+        if 'Connection refused' in exc:
+            print 'Connection refused by server... Sleeping 1 seconds...'
+            time.sleep(1)
+    def handle_group_join(self, conn, group, member, cause):
+        print 'Another HB server joined: "%s" joined. Reason: %s' % (member, cause)
+    def handle_group_leave(self, conn, group, member, cause):
+        print 'A HB server LEFT: "%s" left. Reason: %s' % (member, cause)
+    def handle_data(self, conn, message):
+        self.hb_count += 1
+        sender = message.sender
+        data = message.data
+        mtype = message.mesg_type
+        print '%s>> HB message, type=%d, sender="%s", message: %s' % (conn.name, mtype, sender, data)
+        # and send a response back
+        conn.unicast(sender, 'HB Reply:: %d received' % (self.hb_count), mtype)
+
 myname = 'HBsrv-%03d' % (int(time.time()*100) % 1000)
-print 'I am', myname
-
-def got_conn(listener, conn):
-    print 'Got connected:', conn, '\nJoining the :HB channel...'
-    conn.join(':HB')
-def got_dropped(listener, conn):
-    print 'Lost connection to spread server on:', conn.name, 'Reconnecting with start_connect()'
-    conn.start_connect()
-def got_error(listener, conn, exc):
-    print 'Got exception/error:', str(exc)
-    if 'Connection refused' in exc:
-        print 'Connection refused by server... Sleeping 1 seconds...'
-        time.sleep(1)
-def hb_join(conn, group, member, cause):
-    print 'Another HB server joined: on group new member: "%s" joined. Reason: %s' % (member, cause)
-def hb_leave(conn, group, member, cause):
-    print 'A HB server LEFT: on group member: "%s" left. Reason: %s' % (member, cause)
-def hb_mesg(conn, message):
-    conn._hb_count += 1
-    sender = message.sender
-    data = message.data
-    mtype = message.mesg_type
-    print '%s>> HB message, type=%d, sender="%s", message: %s' % (conn.name, mtype, sender, data)
-    # and send a response back
-    conn.unicast(sender, 'HB Reply:: %d received' % (conn._hb_count), mtype)
-gcb_hb = GroupCallback(cb_data=hb_mesg, cb_join=hb_join, cb_leave=hb_leave)
-
-listener = CallbackListener(cb_conn=got_conn, cb_dropped=got_dropped, cb_error=got_error)
-listener.set_group_cb(':HB', gcb_hb)
+hb_listener = HeartbeatServer(myname)
 
 (host, port) = ('localhost', 24999)
 if len(sys.argv) > 1: host = sys.argv[1]
 if len(sys.argv) > 2: port = int(sys.argv[2])
-hb_srv = AsyncSpread(myname, host, port, listener=listener, start_connect=True)
-print 'hb_srv is:', hb_srv
-hb_srv._hb_count = 0
+hbs = AsyncSpread(myname, host, port, listener=hb_listener, start_connect=True)
+print 'hbs is:', hbs
 
 loop=0
 while loop < 10000:
     print '%s: client top of loop %d' % (myname, loop)
     loop += 1
-    hb_srv.run(10)
+    hbs.run(10)
